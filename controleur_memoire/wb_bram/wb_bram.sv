2df6d84e32eaa44516828e6c0aba62391b08f817
Briand Wamba Foukmeniok <bwamba@telecom-paristech.fr>
2018-12-13 19:22:09 +0100

    ajourne

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -2,8 +2,9 @@
 // Wishbone BlockRAM
 //-----------------------------------------------------------------
 //
-// Le paramètre mem_adr_width doit permettre de déterminer le nombre 
+// Le paramètre mem_adr_width doit permettre de déterminer le nombre
 // de mots de la mémoire : (2048 pour mem_adr_width=11)
+
 
 module wb_bram #(parameter mem_adr_width = 11) (
       // Wishbone interface
@@ -11,36 +12,58 @@ module wb_bram #(parameter mem_adr_width = 11) (
       );
       // a vous de jouer a partir d'ici
 	// avec le cicle always_ff nous avons un processus synchrone sur( l'orloge clk )
-	// dont si je fait une affectation avec <= elle aura lieu 1 cicle de clock apres 
-	
-	
-	logic [ 3: 0][ 7:0] memoire [ 2**mem_adr_width]; 
+	// dont si je fait une affectation avec <= elle aura lieu 1 cicle de clock apres
 
-	always_ff( posedge wb_s.clk )
+
+	logic [ 3: 0][ 7:0] memoire [0: ( (1<<mem_adr_width) -1) ]; // nous avons 2^mem_adr.. Mots et chaque mot vaut 4 octects  et 1 octet vaut 8 bits
+
+	//always_comb();
+
+	always_ff @( posedge wb_s.clk )  //or negedge wb_s.rst
 	begin
 
-		if( wb_s.stb && !wb_s.we) begin               // pour une lecture wb_s.we est à zero
-			wb_s.dat_sm <= memoire[ wb_s.adr ];
-			wb_s.ack <= 1`b1 ;
+		// si nous sommes en mode écriture
 
+		if( wb_s.stb && wb_s.we) begin               // pour une ecriture wb_s.we est à un  mem[(wb_s.adr-offset)/4][0] <= wb_s.dat_ms[7:0];
+
+			if( wb_s.sel[0] == 1 ) begin
+				memoire[ wb_s.adr ][ 0 ] <= wb_s.dat_ms[7:0] ;
+			end
+
+			if( wb_s.sel[1] == 1 ) begin
+				memoire[ wb_s.adr ][ 1 ] <= wb_s.dat_ms[15:8] ;
+			end
+
+			if( wb_s.sel[2] == 1 ) begin
+				memoire[ wb_s.adr ][ 2 ] <= wb_s.dat_ms[23:16] ;
+			end
+
+			if( wb_s.sel[3] == 1 ) begin
+				memoire[ wb_s.adr ][ 3 ] <= wb_s.dat_ms[31:24] ;
+			end
 		end
+
+
+		// si nous sommes en mode lecture
+
+		else if( wb_s.stb && !wb_s.we )begin
+			wb_s.dat_sm <= memoire[ wb_s.adr ] ;
+			wb_s.ack <= 1'b1 ;
+			//  QUAND EST-CE QUE wb_s.ack VA REDEVENIR 1 ?
+		end
+
+
 
 	end
 
 
-	always_ff( posedge wb_s.clk )
-	begin
-
-		if( wb_s.stb && !wb_s.we) begin               // pour une lecture wb_s.we est à zero
-			wb_s.ack <= 1`b1 ;
-
-		end
-
-	end
 
 
-   
+
       endmodule
+
+
+
 /*
 
   // Modport for slave rtl
@@ -63,3 +86,4 @@ module wb_bram #(parameter mem_adr_width = 11) (
 
 
 */
+

efba9dcc24a5192866315e1bb5010e60f320acac
Briand Wamba Foukmeniok <bwamba@telecom-paristech.fr>
2018-12-13 22:32:57 +0100

    version1

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -15,40 +15,87 @@ module wb_bram #(parameter mem_adr_width = 11) (
 	// dont si je fait une affectation avec <= elle aura lieu 1 cicle de clock apres
 
 
-	logic [ 3: 0][ 7:0] memoire [0: ( (1<<mem_adr_width) -1) ]; // nous avons 2^mem_adr.. Mots et chaque mot vaut 4 octects  et 1 octet vaut 8 bits
+	logic [ 3: 0][ 7:0] memoire [0: ( (1<<mem_adr_width+10) -1) ]; // nous avons 2^mem_adr.. Mots et chaque mot vaut 4 octects  et 1 octet vaut 8 bits
 
-	//always_comb();
+	//always_comb
+	logic read ,write;
 
 	always_ff @( posedge wb_s.clk )  //or negedge wb_s.rst
 	begin
 
+
+// geron l'ecriture
+
+		if( wb_s.stb ==1 && wb_s.we ==1 ) begin
+
+			if( write == 1  ) begin 
+				write = 0 ;
+				wb_s.ack = 1'b0 ;
+			end 
+			else begin 
+				write = 1 ;
+				wb_s.ack = 1'b1 ;
+			end
+		end
+		else if (wb_s.stb ==0)begin
+			write = 0 ;
+			read = 0 ;
+			wb_s.ack = 1'b0 ;
+		end
+
+
+
+
+
+
 		// si nous sommes en mode écriture
 
-		if( wb_s.stb && wb_s.we) begin               // pour une ecriture wb_s.we est à un  mem[(wb_s.adr-offset)/4][0] <= wb_s.dat_ms[7:0];
+		if( write ==1 ) begin               // pour une ecriture wb_s.we est à un  mem[(wb_s.adr-offset)/4][0] <= wb_s.dat_ms[7:0];
 
 			if( wb_s.sel[0] == 1 ) begin
-				memoire[ wb_s.adr ][ 0 ] <= wb_s.dat_ms[7:0] ;
+				memoire[ (wb_s.adr >>2)  ][ 0 ] = wb_s.dat_ms[7:0] ;
 			end
 
 			if( wb_s.sel[1] == 1 ) begin
-				memoire[ wb_s.adr ][ 1 ] <= wb_s.dat_ms[15:8] ;
+				memoire[ (wb_s.adr >>2) ][ 1 ] = wb_s.dat_ms[15:8] ;
 			end
 
 			if( wb_s.sel[2] == 1 ) begin
-				memoire[ wb_s.adr ][ 2 ] <= wb_s.dat_ms[23:16] ;
+				memoire[ (wb_s.adr >>2) ][ 2 ] = wb_s.dat_ms[23:16] ;
 			end
 
 			if( wb_s.sel[3] == 1 ) begin
-				memoire[ wb_s.adr ][ 3 ] <= wb_s.dat_ms[31:24] ;
+				memoire[ (wb_s.adr >>2) ][ 3 ] = wb_s.dat_ms[31:24] ;
 			end
 		end
 
 
 		// si nous sommes en mode lecture
 
-		else if( wb_s.stb && !wb_s.we )begin
-			wb_s.dat_sm <= memoire[ wb_s.adr ] ;
+		else if( wb_s.stb ==1 && wb_s.we ==0 )begin
+
+			// @(wb_s.dat_ms) begin
+
+			if( wb_s.sel[0] == 1 ) begin
+				wb_s.dat_sm[7:0] <= memoire[ (wb_s.adr >>2) ][ 0 ]  ;
+			end
+
+			if( wb_s.sel[1] == 1 ) begin
+				wb_s.dat_sm[15:8] <= memoire[ (wb_s.adr >>2) ][ 1 ]  ;
+			end
+
+			if( wb_s.sel[2] == 1 ) begin
+				wb_s.dat_sm[23:16] <= memoire[ (wb_s.adr >>2) ][ 2 ]  ;
+			end
+
+			if( wb_s.sel[3] == 1 ) begin
+				wb_s.dat_sm[31:24] <= memoire[ (wb_s.adr >>2) ][ 3 ]  ;
+			end
 			wb_s.ack <= 1'b1 ;
+			if(wb_s.ack == 1) begin
+				wb_s.ack <= 1'b0 ;
+			end
+			//end
 			//  QUAND EST-CE QUE wb_s.ack VA REDEVENIR 1 ?
 		end
 

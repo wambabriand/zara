71f9ba8992a5e0517e7bc6167c1d0f699536bd17
wamba foukmeniok briand <bwamba@telecom-paristech.fr>
2018-12-17 19:43:11 +0100

    test

---
+++ median/src/MED.sv
@@ -0,0 +1,27 @@
+
+
+module MED #(parameter N = 7, M = 8)(
+ input [N:0]  DI, 
+ input BYP,  clk, DSI ,
+ output logic [N:0] D
+) ;
+
+
+
+wire [N:0] w0,w1,w2,w3,w4,w5,w6,w7 ;
+int i;
+
+
+always @(clk)
+
+  begin
+
+	for(i=0;i<M;i++){
+		
+	}
+	
+
+
+  end
+
+endmodule
---
+++ median/src/PIXEL.sv
@@ -0,0 +1,14 @@
+
+module PIXEL #parameter( N = 8 ) (
+
+		input [N:0] I ,
+		input [N:0] O ,
+		output  clk
+) ;
+
+
+
+
+
+
+endmodule 
